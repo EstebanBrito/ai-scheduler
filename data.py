2f2bef08dd4f99bb1cb87d867986f31621c8782d
EstebanBrito <esteban.brito.borges@gmail.com>
2021-06-02 21:00:08 -0500

    Modified data structures

--- data.py
+++ data.py
@@ -1,75 +1,21 @@
 
 
 professors = {
-    'Nora': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
-    'Lucy': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
-    'Mauricio': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
-    'Fatima': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
-    'Maria': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
-    'Glendy': {
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,15],
-            5: [10,11,12,13,14]
-        }
-    },
     'Karime': {
-        'workhours': {
-            1: (7,14),
-            2: (7,14),
-            3: (7,16),
-            4: (7,17),
-            5: (10,15)
-        },
-        'av_worhours': {
-            1: [7,8,9,10,11,12,13],
-            2: [7,8,9,10,11,12,13],
-            3: [7,8,9,10,11,12,13,14,15],
-            4: [7,8,9,10,11,12,13,14,15,16],
-            5: [10,11,12,13,14]
-        }
+        'workhours': [
+            (7,14),
+            (7,14),
+            (7,16),
+            (7,17),
+            (10,15)
+        ],
+        'av_worhours': [
+            [7,8,9,10,11,12,13],
+            [7,8,9,10,11,12,13],
+            [7,8,9,10,11,12,13,14,15],
+            [7,8,9,10,11,12,13,14,15,16],
+            [10,11,12,13,14]
+        ]
     }
 }
 
@@ -80,52 +26,7 @@ courses = {
         'professors':{
             '1SA': 'Nora',
             '1SB': 'Nora',
-            '1SC': 'Aida'
-        }
-    },
-    'Investigacion': {
-        'semester': 1,
-        'weekly_hours': 5,
-        'professors':{
-            '1SA': 'Mauricio',
-            '1SB': 'Aida',
-            '1SC': 'Mauricio'
-        }
-    },
-    'Etica': {
-        'semester': 1,
-        'weekly_hours': 4,
-        'professors':{
-            '1SA': 'Fatima',
-            '1SB': 'Fatima',
-            '1SC': 'Fatima'
-        }
-    },
-    'Calculo': {
-        'semester': 1,
-        'weekly_hours': 5,
-        'professor':{
-            '1SA': 'Maria',
-            '1SB': 'Maria',
-            '1SC': 'Glendy'
-        }
-    },
-    'Administracion': {
-        'semester': 1,
-        'weekly_hours': 5,
-        'professors':{
-            '1SA': 'Fatima',
-            '1SB': 'Fatima',
-            '1SC': 'Fatima'
-        }
-    },
-    'MatDiscretas': {
-        'semester': 1,
-        'weekly_hours': 5,
-        'professors':{
-            '1SA': 'Karime',
-            '1SB': 'Karime',
-            '1SC': 'Karime'
+            '1SC': 'Nora'
         }
     }
 }
@@ -137,35 +38,29 @@ groups = {
             'PROG-SES01': {
                 'course': 'Programacion',
                 'length': 3,
-                'scheduled': False
-            },
-            'INV-SES01': {
-                'course': 'Investigacion',
-                'length': 2,
-                'scheduled': False
-            },
-            'INV-SES01': {
-                'course': 'Investigacion',
-                'length': 2,
-                'scheduled': False
-            },
+                'scheduled': True
+            }
         },
-        'schedule': {
-            1: {},
-            2: {},
-            3: {},
-            4: {},
-            5: {}
-        },
+        'schedule': [
+            [ ['PROG-SES01', [7,10]] ],
+            [],
+            [],
+            [],
+            []
+        ],
         'current_time': {
             'day': 1,
             'hour': 7
         },
         'hour_range': [7,15],
+        'class_hours': [0,0,0,0,0],
+        'weekly_class_hours': 25,
+        'min_daily_class_hours': 0,
         'solved': False
     }
 }
 
 config: {
-    'max_idle_hours': 2
+    'school_workhours': (7,20),
+    'max_daily_idle_hours': 2
 }
